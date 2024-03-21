388b19d5c9141a6c0deca6f458d348bf3d7c214d
rovicz <roviczzf@gmail.com>
2024-03-21 15:13:36 -0300

    adjusts

--- Quiz.java
+++ Quiz.java
@@ -4,11 +4,11 @@ import java.util.Random;
 public class Quiz {
     public static void main(String[] args) {
         String[][] questions = {
-            {"Qual o nome da personagem principal do Life is Strange 1?", "Max Caulfield"},
-            {"Quem possui o cabelo azul na franquia Life is Strange?", "Chloe Price"},
-            {"Em que cidade se passa o Life is Strange 1?", "Arcadia Bay"},
-            {"Qual a data (somente ano) de lançamento do prímeiro episódio de Life is Strange 1?", "2015"},
-            {"Qual o nome do último jogo lançado pela franquia Life is Strange?", "True Colors"}
+            {"Qual o nome da personagem principal do Life is Strange 1?", "B", "A) Chloe Price", "B) Max Caulfield", "C) Rachel Amber", "D) Nathan Prescott"},
+            {"Quem possui o cabelo azul na franquia Life is Strange?", "A", "A) Chloe Price", "B) Max Caulfield", "C) Rachel Amber", "D) Nathan Prescott"},
+            {"Em que cidade se passa o Life is Strange 1?", "C", "A) San Francisco", "B) Los Angeles", "C) Arcadia Bay", "D) Nevada"},
+            {"Qual a data (somente ano) de lançamento do prímeiro episódio de Life is Strange 1?", "C", "A) 2013", "B) 2018", "C) 2015", "D) 2016"},
+            {"Qual o nome do último jogo lançado pela franquia Life is Strange?", "A", "A) True Colors", "B) Before the Storm", "C) After the Storm", "D) The Awesome Adventures of Captain Spirit"}
         };
 
         Scanner scanner = new Scanner(System.in);
@@ -30,11 +30,14 @@ public class Quiz {
             String[] question = questions[index];
 
             System.out.println("Pergunta " + (i + 1) + ": " + question[0]);
+            for (int j = 2; j < question.length; j++) {
+                System.out.println(question[j]);
+            }
 
-            System.out.print("Sua resposta: ");
-            String answer = scanner.nextLine();
+            System.out.print("Sua resposta (A, B, C ou D): ");
+            String answer = scanner.nextLine().toUpperCase();
 
-            if (answer.equalsIgnoreCase(question[1])) {
+            if (answer.equals(question[1])) {
                 System.out.println("Resposta correta!");
                 score++;
             } else {
